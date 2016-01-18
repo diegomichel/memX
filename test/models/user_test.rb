14cb90ff506d3df8abf7a275d7acb4247105e735
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-17 22:45:37 -0600

    Test for user email uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new({name:'juan', email: 'juan@juan.com'})
+  end
+
+  test "model validations" do
+    duplicated_user = @user.dup
+    duplicated_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicated_user.valid?
+  end
 end
