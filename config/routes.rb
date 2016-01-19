3e16df13a58bce244156163268e673ba06c530f6
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-19 02:03:26 -0600

    Moves tests as nested to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tests
   resources :listitems
-  resources :lists
+  resources :lists do
+    resources :tests
+  end
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
