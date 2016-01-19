3f1210756eb9d4737ed88b212c0287620ddc9150
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-19 00:10:58 -0600

    Better list listing, buttons removed and using some bootstrap

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,10 +1,15 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :list_items
+  has_many :listitems
   has_many :tests
   def text
+    items = listitems.pluck(:text)
+    items.join("\r\n")
   end
 
   def text=(val)
+    val.split("\r\n").each do |item|
+      listitems.create!({text: item, previous_item_id: listitems.last ?  listitems.last.id : nil })
+    end
   end
 end
