3f38b35c79ebf85642d46d11d288e6eaf9d84cca
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-20 22:22:19 -0600

    now uses build from list, also syntax improvements, and some disables for rubocop

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,3 +1,4 @@
+# Controller for tests, where tests are tests over our memorizable lists
 class TestsController < ApplicationController
   before_action :set_test, only: [:show, :edit, :update, :destroy]
 
@@ -15,7 +16,7 @@ class TestsController < ApplicationController
   # GET /tests/new
   def new
     @list = List.find_by_id(params['list_id'])
-    @test = Test.new
+    @test = @list.tests.build
   end
 
   # GET /tests/1/edit
@@ -24,12 +25,14 @@ class TestsController < ApplicationController
 
   # POST /tests
   # POST /tests.json
-  def create
+  def create # rubocop:disable all
     @test = Test.new(test_params)
-
     respond_to do |format|
       if @test.save
-        format.html { redirect_to @test, notice: 'Test was successfully created.' }
+        format.html do
+          redirect_to list_test_path(@test.list, @test),
+                      notice: 'Test was successfully created.'
+        end
         format.json { render :show, status: :created, location: @test }
       else
         format.html { render :new }
@@ -40,10 +43,12 @@ class TestsController < ApplicationController
 
   # PATCH/PUT /tests/1
   # PATCH/PUT /tests/1.json
-  def update
+  def update # rubocop:disable Metrics/MethodLength
     respond_to do |format|
       if @test.update(test_params)
-        format.html { redirect_to @test, notice: 'Test was successfully updated.' }
+        format.html do
+          redirect_to @test, notice: 'Test was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @test }
       else
         format.html { render :edit }
@@ -57,19 +62,23 @@ class TestsController < ApplicationController
   def destroy
     @test.destroy
     respond_to do |format|
-      format.html { redirect_to tests_url, notice: 'Test was successfully destroyed.' }
+      format.html do
+        redirect_to tests_url, notice: 'Test was successfully destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_test
-      @test = Test.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def test_params
-      params.require(:test).permit(:list_id, :text)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def test_params
+    params.require(:test).permit(:list_id, :text)
+  end
 end
