d6660a11c4afae6a431b6a2f814bec09a6269494
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-21 00:20:26 -0600

    Test model implementation and views

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,20 @@
 class Test < ActiveRecord::Base
   belongs_to :list
   validates :list_id, presence: true
+
+  def diff
+    Diffy::SplitDiff.new(text,
+                         list.text,
+                         diff: ["-U 10000", "-w"],
+                         format: :html,
+                         include_plus_and_minus_in_html: true,
+                         allow_empty_diff: false)
+  end
+
+  def score
+    lines = list.text.count("\\n\\r")
+    unchanged = diff.left.count("unchanged")
+    inserts = diff.left.count("ins")
+    deletes = diff.left.count("del")
+  end
 end
