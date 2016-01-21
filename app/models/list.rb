a3479d54153e2273a8524c5cfd22ae872e815cca
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-01-20 21:02:30 -0600

    Syntax improved, validates prescence of user_id

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,15 +1,24 @@
+# A list contains the items to memorize
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :listitems
   has_many :tests
+
+  validates :user_id, presence: true
+
   def text
     items = listitems.pluck(:text)
     items.join("\r\n")
   end
 
-  def text=(val)
-    val.split("\r\n").each do |item|
-      listitems.create!({text: item, previous_item_id: listitems.last ?  listitems.last.id : nil })
+  def text=(text)
+    text.split("\r\n").each do |item|
+      listitems.create!(text: item,
+                        previous_item_id: last_item_id)
     end
+  end
+
+  def last_item_id
+    listitems.last ? listitems.last.id : nil
   end
 end
